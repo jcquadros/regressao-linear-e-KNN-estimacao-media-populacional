c4701973f020c55cb775f8b3b83ea5ffa42e009d
shsys <jhaweoyuo>
2023-10-30 14:30:21 -0300

    finalizado

--- regressao.py
+++ regressao.py
@@ -5,17 +5,16 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
 
-# Passo 1: Carregue os dados do CSV
-data = pd.read_csv('seuarquivo.csv')  # Substitua 'seuarquivo.csv' pelo nome do seu arquivo CSV
+data = pd.read_csv('Trabalho.csv')  
 
-# Passo 2: Separe o conjunto em treino e teste
+# Separando o conjunto em treino e teste
 X = data[['X1', 'X2', 'X3', 'X4']]
 Y = data['Y']
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
 
-# Passo 3: Ajuste modelos KNN e regressão linear e calcule o erro de previsão
+# Ajustando modelos KNN e regressão linear e calcule o erro de previsão
 k_values = [1, 3, 5]
-errors = {'KNN_1': [], 'KNN_3': [], 'KNN_5': [], 'LinearRegression': []}
+errors = {'KNN_1': [], 'KNN_3': [], 'KNN_5': [], 'Regressão_Linear': []}
 
 for k in k_values:
     knn = KNeighborsRegressor(n_neighbors=k)
@@ -26,17 +25,16 @@ for k in k_values:
 lr = LinearRegression()
 lr.fit(X_train, Y_train)
 Y_pred = lr.predict(X_test)
-errors['LinearRegression'] = np.abs(Y_test - Y_pred)
+errors['Regressão_Linear'] = np.abs(Y_test - Y_pred)
 
-# Passo 4: Estime o erro absoluto médio (MAE) para cada método
-mae_results = {}
+# Estimando o erro absoluto médio para cada método
+erro_results = {}
 for method, error in errors.items():
-    mae = np.mean(error)
-    mae_std = np.std(error)
-    mae_results[method] = {'MAE': mae, 'MAE_std': mae_std}
+    erro_medio = np.mean(error)
+    erro_std = np.std(error)
+    erro_results[method] = {'erro absoluto médio': erro_medio, 'desvio padrao': erro_std}
 
-# Passo 5: Conclusões sobre os resultados
-for method, result in mae_results.items():
-    print(f'{method}: MAE = {result["MAE"]:.2f} (Desvio Padrão = {result["MAE_std"]:.2f})')
+# ResultadosS
+for method, result in erro_results.items():
+    print(f'{method}: erro absoluto médio = {result["erro absoluto médio"]:.2f} (Desvio Padrão = {result["desvio padrao"]:.2f})')
 
-# Você também pode comparar os modelos com base nos resultados de MAE e escolher o melhor método.
